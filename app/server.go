bf9f6bc314e23ff19bdacf3e326b19378331bfc1
Bob Yang <Bobyang@Bobs-MacBook-Pro.local>
2020-10-02 16:43:41 +0800

    allow CROS

--- app/server.go
+++ app/server.go
@@ -1,12 +1,14 @@
 package app
 
 import (
+	"os"
 	"todo/app/database"
 	"todo/app/validation"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 // Server godoc
@@ -19,6 +21,13 @@ type Server struct {
 func NewServer() *Server {
 	e := echo.New()
 	e.Validator = &validation.CustomValidator{Validator: validator.New()}
+	if os.Getenv("ENV") == "dev" {
+		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowCredentials: true,
+			AllowMethods:     []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+			AllowOrigins:     []string{"*"},
+		}))
+	}
 	return &Server{
 		Echo: e,
 		DB:   database.InitDB(),
